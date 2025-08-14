5a5fd10e1f696744144d6a6c32e4691a961199c0
dubeyjiongit <dubeyjicodes@gmail.com>
2025-08-14 10:58:27 +0000

    my starting codes

---
+++ 1st/variables.js
@@ -0,0 +1,12 @@
+const accountID = 144553
+let accountEmail = "sarvesh@gmail.com"
+var accountPassword = "12345"
+//accountID = "jaipur"
+
+//accountID = 2  //not allowed kyuki const me upr diya h
+
+accountEmail = "hch@gmail.com";
+
+accountCity = "bengaluru"
+accountPassword = "2121"
+console.table([accountID, accountEmail, accountPassword, accountCity]);
